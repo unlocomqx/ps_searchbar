d61f3820a05f9dd5ff29853e2fce7846683f7aa4
unlocomqx <amxmodx.unloco@gmail.com>
2017-01-20 23:01:11 +0100

    license version

--- ps_searchbar.php
+++ ps_searchbar.php
@@ -4,7 +4,7 @@
 *
 * NOTICE OF LICENSE
 *
-* This source file is subject to the Academic Free License (AFL 3.0)
+* This source file is subject to the Academic Free License (AFL 4.0)
 * that is bundled with this package in the file LICENSE.txt.
 * It is also available through the world-wide-web at this URL:
 * http://opensource.org/licenses/afl-3.0.php
